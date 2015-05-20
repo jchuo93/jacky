9167fb69251da3f0ba42a1475aa8410760d375ed
jchuo93 <jackychuo93@gmail.com>
2015-05-20 12:01:26 -0700

    edited leaderboard image

--- Preloader.js
+++ Preloader.js
@@ -1,12 +1,12 @@
 
 /**
- * Description
+ * This script is used to preload all the assets,  
+ * after preloading, the game page will load faster when refreshed. 
  * @method Preloader
- * @param {} game
- * @return 
+ * @param game
  */
 FindX.Preloader = function(game) {
-    this.preloadBar = null;
+    this.preloadBar = null; //preloader bar to show the status of asset loading 
     this.titleText = null;
     this.ready = false;
 };
@@ -14,50 +14,55 @@ FindX.Preloader = function(game) {
 FindX.Preloader.prototype = {
 	
 	/**
-	 * Description
+	 * This function creates the preloader bar and loads all the assets. 
 	 * @method preload
-	 * @return 
 	 */
 	preload: function () {
 		this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY + 250, 'preloaderBar');
 		this.preloadBar.anchor.setTo(0.5, 0);
 		this.load.setPreloadSprite(this.preloadBar);
         this.load.image('titlescreen', 'images/bgPirateMap.jpg');
-        this.load.image('titlelogo', 'images/newfindxps.png');
+        this.load.image('titlelogo', 'images/logo.png');
+        this.load.bitmapFont('boardfont', 'font/board.png', 'font/board.fnt');
         this.load.bitmapFont('gamefont', 'font/font.png', 'font/font.fnt');
         this.load.spritesheet('PlayButton', 'images/play_spritesheet.png', 159, 82);
         this.load.spritesheet('Settings', 'images/Settings.png', 273, 82);
-        this.load.spritesheet('HighScore', 'images/highscore.png', 350, 82);
-        this.load.audio('select_audio', 'audio/select.mp3');
-        this.load.audio('wrong_audio', 'audio/argh.mp3');
-        this.load.audio('coin_audio', 'audio/coinbag.mp3');
-        this.load.audio('start_audio', 'audio/bgLoop.mp3');
+        this.load.spritesheet('HighScore', 'images/leaderboardButton.png', 406, 68);
+        this.load.spritesheet('Achievement', 'images/achievement.png', 437, 62);
         this.load.image('settingsBG', 'images/settingsBG.jpg');
-        this.load.image('pause','images/PAUSE.png');
+        this.load.image('settingsBG3', 'images/settingsBG3.png');
         this.load.spritesheet('skip','images/skipSprite.png', 142, 142);
+        this.load.spritesheet('buttonChoice','images/colorButtons.png', 167, 167);
         this.load.image('loot','images/coinPouch.png');
         this.load.image('gameoverskull','images/GameOver.png');
         this.load.image('gameoverplay','images/GameOverPLAY.png');
         this.load.image('gameoverquit','images/GameOverQUIT.png');
         this.load.image('wheelBanner','images/wheel.png');
+        this.load.image('submitButton','images/submitButton.png');
+        this.load.image('correct','images/green.png');
+        this.load.image('wrong','images/wrong.png');
+        this.load.image('lock','images/lock.png');
+        this.load.image('bronze','images/bronze.png');
+        this.load.audio('select_audio', 'audio/select.mp3');
+        this.load.audio('wrong_audio', 'audio/argh.mp3');
+        this.load.audio('coin_audio', 'audio/coinbag.mp3');
+        this.load.audio('start_audio', 'audio/bgLoop.mp3');
 	},
 	
 
 	/**
-	 * Description
+	 * Creates the page and enables resizability.
 	 * @method create
-	 * @return 
 	 */
 	create: function () {
-		this.preloadBar.cropEnabled = false;
-        
+		this.preloadBar.cropEnabled = false; 
         
 	},
 
 	/**
-	 * Description
+	 * function that updates the page after loading and 
+     * changes the sate of the page to start. 
 	 * @method update
-	 * @return 
 	 */
 	update: function () {
         if(this.cache.isSoundDecoded('start_audio') && this.ready == false) {
--- StartMenu.js
+++ StartMenu.js
@@ -1,8 +1,19 @@
 /**
+* @author       Rupinder Sandhu, Luda Shu, Manish Mallavarapu, Jacky Chou, Jox Toyod (Team 10 / 5 bits) 
+* @version      0.7
+*
+* Start Menu for the game, includes clickable buttons for play,
+* leaderboard and settings. The play button leads to the game
+* Javascript page, the leaderboard button creates a window 
+* which includes the name and scores for each person that
+* has submitted a score. Lastly, the settings button creates a 
+* window for turning the volume on or off.
+*/
+
+/**
  * Shows the Start Menu Interface with designation clickable buttons
  * @method StartMenu
- * @param {} game
- * @return 
+ * @param {} game Variables for the Start Menu screen
  */
 FindX.StartMenu = function(game) {
     this.logo;
@@ -11,80 +22,316 @@ FindX.StartMenu = function(game) {
     this.ding;
     this.startSound;
     this.settingsButton;
+    this.achievementButton;
     this.onSound;
     this.soundToggle;
-    
-  
 }
 
 
 FindX.StartMenu.prototype = {
 	
 	/**
-	 * Creates the buttons
-	 * @method create
-	 * @return 
+	 * Creates the buttons, sounds and database needed for leaderboard
+	 * @method create Functions for the Start Menu screen
 	 */
 	create: function () {
         this.onSound = true;
-        
+        this.stage.disableVisibilityChange = false;
         this.ding = this.add.audio('select_audio');
         this.startSound = this.add.audio('start_audio');
         this.startSound.play('', 0, 0.8, true);
 		this.add.image(0, 0, 'titlescreen');
         this.logo = this.add.image(this.world.centerX - 230, this.world.centerY - 400, 'titlelogo');
-        highScoreButton = this.add.button(this.world.centerX, this.world.centerY+150, 'HighScore', this.highScore, this, 1 , 0);
+        
+        highScoreButton = this.add.button(this.world.centerX, this.world.centerY+150, 'HighScore', this.highScoreTween, this, 1 , 0);
 		startPrompt = this.add.button(this.world.centerX, this.world.centerY+80, 'PlayButton', this.startGame, this, 1 , 0);
-        settingsButton = this.add.button(this.world.centerX, this.world.centerY+220, 'Settings', this.settingsMenu, this, 0 , 1);
+        settingsButton = this.add.button(this.world.centerX, this.world.centerY+300, 'Settings', this.settingsTween, this, 0 , 1);
+        achievementButton = this.add.button(this.world.centerX, this.world.centerY+220, 'Achievement', this.achieveTween, this, 1 , 0);
+        
         startPrompt.anchor.setTo(0.5, 0.5);
         highScoreButton.anchor.setTo(0.5, 0.5);
         settingsButton.anchor.setTo(0.5, 0.5);
+        achievementButton.anchor.setTo(0.5, 0.5);
         
-       
+        //get the top 10 score from the database and store in the local storage
+        $.ajax({
+           url:  'https://api.mongolab.com/api/1/databases/findx/collections/HighScore?s={\"score\":-1}&l=10&apiKey=CDvbQJBiWFpyu08aN2PYkWAqi2Q3m0E1',
+            dataType: 'json',
+            type: 'get',
+            cache: false,
+            success: function(data) {
+                     
+                for(var i = 0; i < 10; i++ ) {
+                    localStorage.setItem('topName' + i , data[i].name);
+                    localStorage.setItem('topScore' + i , data[i].score);
+                }       
+            }
+                   
+        });
+        
+    
 	},
+    
+    /**
+    * Creates the achievement display
+    *
+    */
+    achieve: function() {
+        
+        startPrompt.inputEnabled = false;
+        highScoreButton.inputEnabled = false;
+        settingsButton.inputEnabled = false;
+        achievementButton.inputEnabled = false;
+        
+        var sprite = this.add.image(this.world.centerX, this.world.centerY+10, 'settingsBG');
+        sprite.anchor.setTo(0.5, 0.5);
+        
+        var achieveText = this.add.bitmapText(0 , 0,'gamefont', 'Achievements', 40);
+        sprite.addChild(achieveText);
+        achieveText.anchor.setTo(0.5, 0.5);
+        achieveText.x = 0;  
+        achieveText.y = -200;
+                
+              
+        var line1 = this.add.bitmapText(this.world.centerX, this.world.centerY+10, 'gamefont', '____________________', 30);
+        sprite.addChild(line1);
+        line1.x = -160;
+        line1.y = -90;
+        
+        // unlock the first achievement
+        if(localStorage.getItem("yourscore") != null){
+         
+                if(parseInt(localStorage.getItem("yourscore")) >= 10){
+                    var achieve1 = this.add.bitmapText(this.world.centerX, this.world.centerY+10, 'gamefont', "you got", 30);
+                    sprite.addChild(achieve1);
+                    achieve1.x = -160;
+                    achieve1.y = -90;
+                    
+                    var bronzeimg = this.add.image(0, 0, 'bronze');
+                    sprite.addChild(bronzeimg);
+                    bronzeimg.x = -120;
+                    bronzeimg.y = -100;
+                } else {
+                    
+                    var lockimg1 = this.createLock(this.world.centerX, this.world.centerY+10, 'lock');
+                    sprite.addChild(lockimg1);
+                    lockimg1.x = -120;
+                    lockimg1.y = -100;
+                }
+        }else{
+            
+               var lockimg1 = this.createLock(this.world.centerX, this.world.centerY+10, 'lock');
+                sprite.addChild(lockimg1);
+                lockimg1.x = -120;
+                lockimg1.y = -100;
+
+        }
+        
+        //achieve 2 ------------------
+        var lockimg2 = this.createLock(this.world.centerX, this.world.centerY+10, 'lock');
+        sprite.addChild(lockimg2);
+        lockimg2.x = -120;
+        lockimg2.y = -10;
+              
+        var line2 = this.add.bitmapText(this.world.centerX, this.world.centerY+10, 'gamefont', '____________________', 30);
+        sprite.addChild(line2);
+        line2.x = -160;
+        line2.y = 0;
+        
+        //achieve 3 -----------------
+        var lockimg3 = this.createLock(this.world.centerX, this.world.centerY+10, 'lock');
+        sprite.addChild(lockimg3);
+        lockimg3.x = -120;
+        lockimg3.y = 80;
+              
+        var line3 = this.add.bitmapText(this.world.centerX, this.world.centerY+10, 'gamefont', '____________________', 30);
+        sprite.addChild(line3);
+        line3.x = -160;
+        line3.y = 90;
+        
+        
+        backToMenu = this.add.bitmapText(0 , 0,'gamefont', 'back', 30);
+        backToMenu.inputEnabled = true;
+        
+        sprite.addChild(backToMenu);
+        backToMenu.anchor.setTo(0.5, 0.5);
+        backToMenu.x = 10;
+        backToMenu.y = 200;
+        
+         backToMenu.events.onInputDown.add(
+                   
+            function() {
+                this.add.tween(backToMenu.scale).to( { x: 1.3, y: 1.3 }, 100, 
+                    Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(
+                        function() {
+                            startPrompt.inputEnabled = true;
+                            highScoreButton.inputEnabled = true;
+                            settingsButton.inputEnabled = true;
+                            achievementButton.inputEnabled = true;
+                            sprite.destroy();
+                            backToMenu.destroy();
+                        }, this)
+                }
+                ,this);
+        
+    },
+    
+    /**
+    * Creates the lock icon
+    * @param xLoc the location of the icon
+    * @return returns the lock icon with specified location
+    */
+    createLock: function(xloc, yloc, key) {
+        
+        var lockimg = this.add.image(xloc, yloc, key);
+        lockimg.width = 75;
+        lockimg.height = 75;    
+        lockimg.anchor.setTo(0.5, 0.5);
+       
+          
+        return lockimg;
+        
+    },
+    
+    /**
+    * Function call when achievement is click
+    * adds a tween to the achievement button
+    */
+    achieveTween: function() {     
+        
+        this.add.tween(achievementButton.scale).to( { x: .8, y: .8 }, 50, Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(
+            this.achieve, this);
+        
+    },
 	/**
 	 * Function call when Play Button is click
-	 * @method startGame
-	 * @param {} pointer
-	 * @return 
+	 * @method startGame Button with a pointer
+	 * @param {} pointer Pointer to the game.js screen
 	 */
 	startGame: function (pointer) {
         this.ding.play();
         this.startSound.stop();
-		this.state.start('Game');
-        
+        this.add.tween(startPrompt.scale).to( { x: .8, y: .8 }, 50, Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(
+            function() {
+                this.state.start('Game');
+            }, this);    
 	},
     
+    /*
+     * Tween the leaderBoard button
+     * then call the settings Menu thereafter
+     */
+    highScoreTween: function() {
+        this.add.tween(highScoreButton.scale).to( { x: .8, y: .8 }, 50, 
+                Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(this.highScore, this); 
+    },
+    
     /**
-     * Function call when High Score Button is click
-     * @method highScore
-     * @param {} pointer
-     * @return 
+     * Function call when LeaderBoard Button is click, includes an online 
+     * leaderboard that pull and posts from mongodb database
+     * @method highScore Button with a pointer
+     * @param {} pointer Points to a window created for leaderboard
      */
     highScore: function(pointer) {
+        
         startPrompt.inputEnabled = false;
         highScoreButton.inputEnabled = false;
         settingsButton.inputEnabled = false;
-         var sprite = this.add.image(this.world.centerX, this.world.centerY+10, 'settingsBG');
-         sprite.anchor.setTo(0.5, 0.5);
+        achievementButton.inputEnabled = false;
         
-         backToMenu = this.add.bitmapText(0 , 0,'gamefont', 'back', 30);
-         backToMenu.inputEnabled = true;
+        var score = [];
+        var name = [];
+        var rank = [];
+        var titleIndex;
+        var sprite = this.add.image(this.world.centerX, this.world.centerY+10, 'settingsBG3');
+        sprite.anchor.setTo(0.5, 0.5);
         
+        //sprite.y = 670;
         
-        highScore = this.add.bitmapText(0 , 0,'gamefont', 'High Score', 40);
+        backToMenu = this.add.bitmapText(0 , 0,'gamefont', 'back', 30);
+        backToMenu.inputEnabled = true;
+         
+        highScore = this.add.bitmapText(0 , 0,'gamefont', 'Leaderboard', 40);
         sprite.addChild(highScore);
         highScore.anchor.setTo(0.5, 0.5);
         highScore.x = 0;  
         highScore.y = -200;
         
-        // get the save high Score from local storage
-        score = this.add.bitmapText(0 , 0,'gamefont', localStorage.getItem('highscore'), 60);
-        sprite.addChild(score);
-        score.anchor.setTo(0.5, 0.5);
-        score.x = 0;  
-        score.y = 0;
+        var yPos = -100;
+        var xPos = 0;
+                    
+        $.ajax({
+           url:  'https://api.mongolab.com/api/1/databases/findx/collections/HighScore?s={\"score\":-1}&l=10&apiKey=CDvbQJBiWFpyu08aN2PYkWAqi2Q3m0E1',
+            dataType: 'json',
+            type: 'get',
+            cache: false,
+            success: function(data) {
+                     
+                for(var i = 0; i < 10; i++ ) {
+                    localStorage.setItem('topName' + i , data[i].name);
+                    localStorage.setItem('topScore' + i , data[i].score);
+                }       
+            }
+                   
+        });
         
+        titleIndex = this.add.bitmapText(0 , 0,'gamefont', 'RANK   ' + 'NAME   ' + 'SCORE' , 30);
+        sprite.addChild(titleIndex);
+        titleIndex.anchor.setTo(0.5, 0.5);
+        titleIndex.x = 0;  
+        titleIndex.y = -150;   
+        
+        for(var i = 0; i < 10; i++, yPos += 28 ) {
+            
+            if (localStorage.getItem('topName' + i) != null) {
+                //rank
+                rank[i] = this.add.bitmapText(0 , 0,'gamefont', "" + (i+1) , 27);
+                sprite.addChild(rank[i]);
+                rank[i].anchor.setTo(0.5, 0.5);
+                rank[i].x = -125;  
+                rank[i].y = yPos;
+
+                //name
+                name[i] = this.add.bitmapText(0 , 0,'boardfont',  "" + localStorage.getItem('topName' + i), 27);
+                sprite.addChild(name[i]);
+                name[i].anchor.setTo(0.5, 0.5);
+                name[i].x = -5;  
+                name[i].y = yPos;
+
+                //score
+                score[i] = this.add.bitmapText(0 , 0,'gamefont',  "" + localStorage.getItem('topScore' + i), 27);
+                sprite.addChild(score[i]);
+                score[i].anchor.setTo(0.5, 0.5);
+                score[i].x = 120;  
+                score[i].y = yPos;
+                
+            }else {
+                
+                //rank
+                rank[i] = this.add.bitmapText(0 , 0,'gamefont', "" + (i+1) , 27);
+                sprite.addChild(rank[i]);
+                rank[i].anchor.setTo(0.5, 0.5);
+                rank[i].x = -125;  
+                rank[i].y = yPos;
+
+                //name
+                name[i] = this.add.bitmapText(0 , 0,'gamefont',  "--", 27);
+                sprite.addChild(name[i]);
+                name[i].anchor.setTo(0.5, 0.5);
+                name[i].x = -5;  
+                name[i].y = yPos;
+
+                //score
+                score[i] = this.add.bitmapText(0 , 0,'gamefont',  "--", 27);
+                sprite.addChild(score[i]);
+                score[i].anchor.setTo(0.5, 0.5);
+                score[i].x = 120;  
+                score[i].y = yPos;     
+            }
+        }
+        
+            
+             
         sprite.addChild(backToMenu);
         backToMenu.anchor.setTo(0.5, 0.5);
         backToMenu.x = 10;
@@ -92,11 +339,16 @@ FindX.StartMenu.prototype = {
         
         backToMenu.events.onInputDown.add(
             function() {
-                startPrompt.inputEnabled = true;
-                highScoreButton.inputEnabled = true;
-                settingsButton.inputEnabled = true;
-                sprite.destroy(); 
-                backToMenu.destroy();
+                this.add.tween(backToMenu.scale).to( { x: 1.3, y: 1.3 }, 100, 
+                    Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(
+                    function() {
+                        startPrompt.inputEnabled = true;
+                        highScoreButton.inputEnabled = true;
+                        settingsButton.inputEnabled = true;
+                        achievementButton.inputEnabled = true;
+                        sprite.destroy(); 
+                        backToMenu.destroy();
+                    }, this)
                 
             }
             ,this);
@@ -104,14 +356,15 @@ FindX.StartMenu.prototype = {
     
     /**
      * Settings Menu with toggle On and off Sound
-     * @method settingsMenu
-     * @param {} pointer
-     * @return 
+     * @method settingsMenu A sound setting menu
+     * @param {} pointer Points to a window created for settings
      */
     settingsMenu : function(pointer){
         startPrompt.inputEnabled = false;
         highScoreButton.inputEnabled = false;
         settingsButton.inputEnabled = false;
+        achievementButton.inputEnabled = false;
+        
         // background Sprite
         var sprite = this.add.image(this.world.centerX, this.world.centerY+10, 'settingsBG');
         sprite.anchor.setTo(0.5, 0.5);
@@ -156,17 +409,34 @@ FindX.StartMenu.prototype = {
         
         // click to go back to menu
         backToMenu.events.onInputDown.add(
+                   
             function() {
-                startPrompt.inputEnabled = true;
-                highScoreButton.inputEnabled = true;
-                settingsButton.inputEnabled = true;
-                soundText.destroy();
-                this.soundToggle.destroy();
-                sprite.destroy();
-                backToMenu.destroy();
-            }
-            ,this);
+                this.add.tween(backToMenu.scale).to( { x: 1.3, y: 1.3 }, 100, 
+                    Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(
+                        function() {
+                            startPrompt.inputEnabled = true;
+                            highScoreButton.inputEnabled = true;
+                            settingsButton.inputEnabled = true;
+                            achievementButton.inputEnabled = true;
+                            soundText.destroy();
+                            this.soundToggle.destroy();
+                            sprite.destroy();
+                            backToMenu.destroy();
+                        }, this)
+                }
+                ,this);
     },
-        
+    
+    /*
+     * Tween the settings button
+     * then call the settings Menu thereafter
+     */
+    settingsTween: function() {
+        this.add.tween(settingsButton.scale).to( { x: .8, y: .8 }, 50, 
+                Phaser.Easing.Linear.None, true, 0, 0, true).onComplete.addOnce(this.settingsMenu, this); 
+    }
+    
+    
+    
      
 };
